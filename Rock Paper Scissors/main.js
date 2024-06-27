83e0c5499d364fe7ff06fc48c21fdf339e9a36bc
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-06-27 21:20:07 +0200

    Rock Paper Scissors update
    Version with UI

--- Rock Paper Scissors/main.js
+++ Rock Paper Scissors/main.js
@@ -16,39 +16,115 @@ let humanScore = 0;
 
 let computerScore = 0;
 
-for (let i = 0; i <= 4; i++) {
-  humanChoice = prompt("Wpisz odpowiedz");
-  computerChoice = getComputerChoice();
-  if (humanChoice === "Rock" || humanChoice === "Paper" || humanChoice === "Scissors") {
-    if (humanChoice === "Rock" && computerChoice === "Rock") {
-      console.log("Draw");
-    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
-      console.log("Win for computer");
-      computerScore++;
-    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-      console.log("Win For human");
-      humanScore++;
-    } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
-      console.log("Win for computer");
-      computerScore++;
-    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-      console.log("Win for human");
-      humanScore++;
-    } else if (humanChoice === "Paper" && computerChoice === "Paper") {
-      console.log("Draw");
-    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-      console.log("Win for human");
-      humanScore++;
-    } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
-      console.log("Win for computer");
-      computerScore++;
-    } else if (humanChoice === "Scissors" && computerChoice === "Scissors") {
-      console.log("Draw");
-    }
-  } else {
-    humanChoice = prompt("Wrong, try again");
-    i--;
-  }
-}
+let start = false;
+const pS = document.querySelector(".humanSc");
+const cS = document.querySelector(".computerSc");
+const reset = document.querySelector(".res");
 
-console.log(`Your score ${humanScore} vs Computer score ${computerScore}`);
+const btnRock = document.querySelector(".rock");
+const btnPaper = document.querySelector(".paper");
+const btnScissors = document.querySelector(".scissors");
+
+reset.addEventListener("click", () => {
+  humanScore = 0;
+  computerScore = 0;
+  cS.textContent = `${computerScore}`;
+  pS.textContent = `${humanScore}`;
+});
+
+btnRock.addEventListener("click", () => {
+  humanChoice = "Rock";
+  start = true;
+  while (start) {
+    if (humanScore + computerScore == 5) {
+      console.log(`Your score ${humanScore} vs computer score ${computerScore} If u want to restart click restart button`);
+      break;
+    }
+    computerChoice = getComputerChoice();
+    if (humanChoice === "Rock" || humanChoice === "Paper" || humanChoice === "Scissors") {
+      if (humanChoice === "Rock" && computerChoice === "Rock") {
+        console.log("Draw");
+        start = false;
+        break;
+      } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        console.log("Win for computer");
+        computerScore++;
+        cS.textContent = `${computerScore}`;
+        start = false;
+        break;
+      } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        console.log("Win For human");
+        humanScore++;
+        pS.textContent = `${humanScore}`;
+        start = false;
+        break;
+      }
+    }
+  }
+});
+
+btnPaper.addEventListener("click", () => {
+  humanChoice = "Paper";
+  start = true;
+  while (start) {
+    if (humanScore + computerScore == 5) {
+      console.log(`Your score ${humanScore} vs computer score ${computerScore} If u want to restart click restart button`);
+      break;
+    }
+    computerChoice = getComputerChoice();
+    if (humanChoice === "Rock" || humanChoice === "Paper" || humanChoice === "Scissors") {
+      if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        console.log("Win for computer");
+        computerScore++;
+        cS.textContent = `${computerScore}`;
+        start = false;
+        break;
+      } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        console.log("Win for human");
+        humanScore++;
+        pS.textContent = `${humanScore}`;
+        start = false;
+        break;
+      } else if (humanChoice === "Paper" && computerChoice === "Paper") {
+        console.log("Draw");
+        start = false;
+        break;
+      } else {
+        i--;
+      }
+    }
+  }
+});
+
+btnScissors.addEventListener("click", () => {
+  humanChoice = "Scissors";
+  start = true;
+  while (start) {
+    if (humanScore + computerScore == 5) {
+      console.log(`Your score ${humanScore} vs computer score ${computerScore}. If u want to restart click restart button`);
+      break;
+    }
+    computerChoice = getComputerChoice();
+    if (humanChoice === "Rock" || humanChoice === "Paper" || humanChoice === "Scissors") {
+      if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        console.log("Win for human");
+        humanScore++;
+        pS.textContent = `${humanScore}`;
+        start = false;
+        break;
+      } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+        console.log("Win for computer");
+        computerScore++;
+        cS.textContent = `${computerScore}`;
+        start = false;
+        break;
+      } else if (humanChoice === "Scissors" && computerChoice === "Scissors") {
+        console.log("Draw");
+        start = false;
+        break;
+      }
+    } else {
+      i--;
+    }
+  }
+});
