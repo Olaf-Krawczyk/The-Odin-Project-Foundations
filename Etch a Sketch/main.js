4fe3a28c0059de912c21bf02b88923310fe7f35d
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-06-28 20:01:43 +0200

    Etch a Sketch

---
+++ Etch a Sketch/main.js
@@ -0,0 +1,20 @@
+const container = document.querySelector(".container");
+
+for (let i = 1; i <= 256; i++) {
+  const div = document.createElement("div");
+  div.classList.add(`${i}`);
+  div.setAttribute("style", "width: 20px; height: 20px; border: 2px solid black");
+
+  container.appendChild(div);
+}
+
+const allDiv = document.querySelectorAll("div");
+
+for (let divs of allDiv) {
+  divs.addEventListener("mouseover", () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    divs.setAttribute("style", `background-color: rgb(${r}, ${g}, ${b})`);
+  });
+}
