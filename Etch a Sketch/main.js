1d67e1ef0967c610088b21f45cfabb39ea8be286
Olaf Krawczyk <olafkrawczyk13@icloud.com>
2024-06-29 12:05:28 +0200

    Fix

--- Etch a Sketch/main.js
+++ Etch a Sketch/main.js
@@ -1,9 +1,30 @@
 const container = document.querySelector(".container");
+const change = document.querySelector(".change");
+let userOption = 16;
 
-for (let i = 1; i <= 256; i++) {
+change.addEventListener("click", () => {
+  let userOption = prompt("Choose number between 1 to 100");
+  if (userOption < 1 || userOption > 100) {
+    console.log("WRONG");
+  } else {
+    userOption = parseFloat(userOption);
+    for (let div of allDiv) {
+      div.remove();
+    }
+    for (let i = 1; i <= userOption * userOption; i++) {
+      const div = document.createElement("div");
+      div.classList.add(`${i}`);
+      div.setAttribute("style", `width: ${320 / userOption}px; height: ${320 / userOption}px; border: 2px solid black`);
+      container.appendChild(div);
+      container.style.gridTemplateColumns = `repeat(${userOption}, ${320 / userOption}px)`;
+    }
+  }
+});
+
+for (let i = 1; i <= userOption * userOption; i++) {
   const div = document.createElement("div");
   div.classList.add(`${i}`);
-  div.setAttribute("style", "width: 20px; height: 20px; border: 2px solid black");
+  div.setAttribute("style", `width: 20px; height: 20px; border: 2px solid black`);
 
   container.appendChild(div);
 }
